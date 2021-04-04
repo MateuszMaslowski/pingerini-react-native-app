829553a78b5b925817e7daf67950dbdafa56d0f4
GrzegorzGruza <grzegorz.gruza00@gmail.com>
2021-04-04 08:06:52 +0200

    expo

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
  * @format
  */
 
+import { registerRootComponent } from 'expo';
+registerRootComponent(App);
+
+
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
